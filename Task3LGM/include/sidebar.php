5ad81e3d6f67d89d8fe06a45a2ec821238996f41
Chinmayadas123 <86675089+Chinmayadas123@users.noreply.github.com>
2021-10-30 20:33:46 +0530

    initial

---
+++ Task3LGM/include/sidebar.php
@@ -0,0 +1,41 @@
+<div class="sidebar">
+    <div class="logo-details">
+      <i class='bx bxl-c-plus-plus icon'></i>
+        <div class="logo_name">Admin</div>
+        <i class='bx bx-menu' id="btn" ></i>
+    </div>
+    <ul class="nav-list">
+     
+      <li>
+        <a href="admin.php">
+          <i class='bx bx-grid-alt'></i>
+          <span class="links_name">Dashboard</span>
+        </a>
+
+      </li>
+      <li>
+       <a href="managestudent.php">
+         <i class='bx bx-user' ></i>
+         <span class="links_name">Student</span>
+       </a>
+     </li>
+     <li>
+      <a href="branch.php">
+        <i class='bx bx-user' ></i>
+        <span class="links_name">Branch</span>
+      </a>
+    </li>
+    <li>
+      <a href="result.php">
+        <i class='bx bx-user' ></i>
+        <span class="links_name">Result</span>
+      </a>
+    </li>
+    <li>
+      <a href="logout.php">
+        <i class='bx bx-user' ></i>
+        <span class="links_name">Logout</span>
+      </a>
+    </li>
+    </ul>
+  </div>
